9f9c578e8ef4bfe8d7e088da349dd12cfb566354
MeganKay45 <130116352+MeganKay45@users.noreply.github.com>
2023-04-11 19:27:01 -0600

    Add files via upload

---
+++ Lesson2Lab1.py
@@ -0,0 +1,14 @@
+x = int(input("Enter 1st number: "))
+y = int(input("Enter 2nd number: "))
+print("sum: ", x+y)
+print("difference: ", x-y)
+print("Multiply: ", x*y)
+print("Divide: ", x/y)
+print("Remainder: ", x%y)
+
+num = 2
+print(type(num))
+num = float(num)
+print(num, type(num))
+
+
